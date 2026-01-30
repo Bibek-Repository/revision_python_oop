e96f1d6eb67970a34cd2fbd043b03bbf7e0844d4
[Bibek Baiju] <{bibekbaiju18@gmail.com]>
2026-01-30 10:22:20 +0545

    inheritance in python

--- oop.py
+++ oop.py
@@ -1,5 +1,22 @@
-class animal:
-    x = 5
+class employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
 
-p1=animal()
-print(p1.x)
+    def display(self):
+        print(f"name of the employee is {self.name} and salary is {self.salary}")
+
+class developer(employee):
+    def __init__(self,name, salary, bonus):
+        self.name = name
+        self.salary = salary
+        self.bonus = bonus
+    
+    def display(self):
+        print(f"developer name: {self.name}, salary of the developer is {self.salary} and bonus of the developer is {self. bonus} ")
+
+
+p1=developer("Bibek", 100000, 20000)
+
+
+    
